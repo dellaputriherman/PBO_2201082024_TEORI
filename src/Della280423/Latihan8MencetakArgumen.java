bf188c45eac22358ed77308840a0cfcc5f15e3c1
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-05-05 11:41:16 +0700

    modul 8

---
+++ src/Della280423/Latihan8MencetakArgumen.java
@@ -0,0 +1,24 @@
+package Della280423;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan8MencetakArgumen {
+     public static void main(String[] args) { 
+                                             
+         for (String arg : args) {
+             System.out.println(arg);
+         }
+         System.out.println("number of array: "+args.length);
+   }
+}
+
+
+    
+
